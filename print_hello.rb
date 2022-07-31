1c6c84d17b41fbc7fc2c8854ff00ce158137e78a
niyaziicdag <niyazi_icdag@hotmail.com>
2022-07-31 22:24:47 +0300

    Add calculate method

--- print_hello.rb
+++ print_hello.rb
@@ -2,11 +2,30 @@ class Hello
   def say_hello(name)
     puts "Hello #{name}"
   end
-  def plus(a,b)
-    return a+b
+end
+class Calculate
+  def plus(a, b)
+    return a + b
+  end
+
+  def subtraction(a, b)
+    return a / b
+  end
+
+  def multiplication(a, b)
+    return a / b
+  end
+
+  def divide(a, b)
+    return a / b
   end
 end
 
-hi=Hello.new
+hi = Hello.new
 hi.say_hello("Neo")
-puts hi.plus(13,5)
+calc=Calculate.new
+
+puts calc.plus(13, 5)
+puts calc.subtraction(13, 5)
+puts calc.multiplication(13, 5)
+puts calc.divide(13, 5)
